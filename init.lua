6b17c9d895ab78913a5e1025ef3b54e6430ff4cd
Braxton <braxtonparker.dev@gmail.com>
2023-06-18 00:44:23 -0500

    tabstop fix

--- init.lua
+++ init.lua
@@ -534,5 +534,11 @@ cmp.setup {
   },
 }
 
+local set = vim.opt
+set.tabstop = 4
+set.softtabstop = 4
+set.shiftwidth = 4
+
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
